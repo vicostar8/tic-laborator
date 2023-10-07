9f493b4f4a65fc247fe967c2b493600e43417cf0
VicoStar <vladchirca8@gmail.com>
2023-10-07 21:11:33 +0300

    Added Laborator route

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import NavigationBar from "./components/navigationBar/navigationBar";
 
 import "./App.css";
 
-
 function App() {
-  return ( 
-    <div className='container container_mainDiv p-3'>
+  return (
+    <div className="container container_mainDiv p-3">
       <NavigationBar />
-      <Routes>
-        <Route path="/" element={<h3>HOME PAGE</h3>} />
-        <Route path="/lectii" element={<h3>LECTII</h3>} />
-        <Route path="/contact" element={<h3>CONTACT</h3>} />
-      </Routes>
+      <div className="p-3 bg-light">
+        <Routes>
+          <Route path="/" element={<h3>HOME PAGE</h3>} />
+          <Route path="/lectii" element={<h3>LECTII</h3>} />
+          <Route path="/laborator" element={<h3>LABORATOR</h3>} />
+          <Route path="/contact" element={<h3>CONTACT</h3>} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/components/navigationBar/navigationBar.jsx
+++ src/components/navigationBar/navigationBar.jsx
@@ -9,11 +9,12 @@ const NavigationBar = () => {
   const menuItems = [
     { nume: "Home", calea: "" },
     { nume: "Lectii", calea: "lectii" },
+    { nume: "Laborator", calea: "laborator" },
     { nume: "Contact", calea: "contact" },
   ];
 
   return (
-    <nav className="navbar navbar-expand-lg navBar_mainDiv">
+    <nav className="navbar navbar-expand-lg navBar_mainDiv py-3">
       <div className="container-fluid">
         <i className="fa-solid fa-school navBar_icon fs-1 mx-3"></i>
 
@@ -21,8 +22,11 @@ const NavigationBar = () => {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            {menuItems.map((mI) => (
-              <NavigationBar_MenuItem mI={mI} />
+            {menuItems.map((mI, idx) => (
+              <NavigationBar_MenuItem
+                key={`${idx}-${mI.nume.toLowerCase()}`}
+                mI={mI}
+              />
             ))}
           </ul>
         </div>

87131a60dd1a02df85643dc4a88a8decdf5d598f
VicoStar <vladchirca8@gmail.com>
2023-10-11 20:55:16 +0300

    Edit Folder lab - cele 3 panele sa fie una sub alta

--- src/components/laborator/laboratorFoldere.jsx
+++ src/components/laborator/laboratorFoldere.jsx
@@ -11,113 +11,109 @@ import move_folder from "../../images/Move_Folder.gif";
 const LaboratorFoldere = () => {
   return (
     <>
-      <div className="row m-0">
-        <div className="fs-4 fw-semibold">
-          Citește cu atenție instrucțiunile și creează următoarele structuri de
-          foldere :
+      <div className="fs-4 fw-semibold">
+        Citește cu atenție instrucțiunile și creează următoarele structuri de
+        foldere :
+        <ul className="fs-5">
+          <li>
+            Apasă pe butonul <strong>START</strong> (
+            <i className="fa-brands fa-microsoft fs-4 mx-2"></i>{" "}
+            <strong>SAU</strong>{" "}
+            <i className="fa-brands fa-windows fs-4 mx-2"></i> ) din colțul
+            stânga al ecranului.
+          </li>
+          <li>
+            Scrii / cauți <strong>FILE EXPLORER</strong> și îl deschizi (apeși
+            pe el click un stânga)
+          </li>
+          <li>
+            În partea stânga, în fâșia îngustă se găsește o listă cu mai multe
+            scurtături. Deschide <strong>DESKTOP</strong> (apeși pe el click un
+            stânga).
+          </li>
+          <li>
+            După ce ai deschis <strong>DESKTOP</strong> creează un folder cu
+            următorul nume: <strong>NUMELE PRENUMELE CLASA</strong>. (exemplu:{" "}
+            <strong>POPESCU ION 7A</strong>)
+          </li>
+        </ul>
+      </div>
+      <div className="p-1">
+        <div className="lab-foldere-note p-1">
+          <div className="fs-4 mb-3 fw-bold text-center">
+            {`Pentru nota 6`.toUpperCase()}
+          </div>
           <ul className="fs-5">
+            <li>Deschide folderul creat cu numele tău</li>
             <li>
-              Apasă pe butonul <strong>START</strong> (
-              <i className="fa-brands fa-microsoft fs-4 mx-2"></i>{" "}
-              <strong>SAU</strong>{" "}
-              <i className="fa-brands fa-windows fs-4 mx-2"></i> ) din colțul
-              stânga al ecranului.
-            </li>
-            <li>
-              Scrii / cauți <strong>FILE EXPLORER</strong> și îl deschizi (apeși
-              pe el click un stânga)
-            </li>
-            <li>
-              În partea stânga, în fâșia îngustă se găsește o listă cu mai multe
-              scurtături. Deschide <strong>DESKTOP</strong> (apeși pe el click
-              un stânga).
-            </li>
-            <li>
-              După ce ai deschis <strong>DESKTOP</strong> creează un folder cu
-              următorul nume: <strong>NUMELE PRENUMELE CLASA</strong>. (exemplu:{" "}
-              <strong>POPESCU ION 7A</strong>)
-            </li>
-          </ul>
-        </div>
-        <div className="col-lg-4 p-1">
-          <div className="lab-foldere-note p-1">
-            <div className="fs-4 mb-3 fw-bold text-center">
-              {`Pentru nota 6`.toUpperCase()}
-            </div>
-            <ul className="fs-5">
-              <li>Deschide folderul creat cu numele tău</li>
-              <li>
-                În folderul cu numele tău creeaza <strong>6</strong> foldere cu
-                următoarele nume:{" "}
-                <strong>LEGUME, FRUCTE, MASINA, AVION, PISICA, CAINE</strong>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="col-lg-4 p-1">
-          <div className="lab-foldere-note p-1">
-            <div className="fs-4 mb-3 fw-bold text-center">
-              {`Pentru nota 8`.toUpperCase()}
-            </div>
-            <ul className="fs-5">
-              <li>
-                Deschide folderul <strong>LEGUME</strong> și creează următoarele{" "}
-                <strong>3</strong> foldere:{" "}
-                <strong>CEAPA, ROSIE, CARTOFI</strong>
-              </li>
-              <li>
-                Navighează înapoi (apasă pe butonul{" "}
-                <i className="fa-solid fa-arrow-left"></i>) din colțul stânga
-                sus
-              </li>
-              <li>
-                Deschide folderul <strong>FRUCTE</strong> și creează următoarele{" "}
-                <strong>3</strong> foldere: <strong>MERE, PERE, BANANE</strong>
-              </li>
-              <li>
-                {" "}
-                Navighează înapoi (apasă pe butonul{" "}
-                <i className="fa-solid fa-arrow-left"></i>) din colțul stânga
-                sus
-              </li>
-              <li>
-                Copiază folderul <strong>PISICA</strong> in folderul{" "}
-                <strong>PERE</strong> (pentru a ajunge în folderul{" "}
-                <strong>PERE</strong> trebuie să navighezi înapoi{" "}
-                <i className="fa-solid fa-arrow-left"></i> în folderul{" "}
-                <strong>FRUCTE</strong> și apoi să intri în folderul{" "}
-                <strong>PERE</strong>)
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="col-lg-4 p-1">
-          <div className="lab-foldere-note p-1">
-            <div className="fs-4 mb-3 fw-bold text-center">
-              {`Pentru nota 10`.toUpperCase()}
-            </div>
-            <ul className="fs-5">
-              <li>Navighează înapoi în folderul cu numele tău</li>
-              <li>
-                Mută folderul <strong>CAINE</strong> in folderul{" "}
-                <strong>ROSIE</strong>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="mt-2">
-          <ul className="fs-5 fw-semibold">
-            <li>După ce ai terminat cheamă-l pe domnul profesor</li>
-            <li>
-              Redenumește folderul <strong>AVION</strong> cu numele{" "}
-              <strong>LACTATE</strong>
-            </li>
-            <li>
-              Șterge folderul <strong>MASINA</strong>
+              În folderul cu numele tău creeaza <strong>6</strong> foldere cu
+              următoarele nume:{" "}
+              <strong>LEGUME, FRUCTE, MASINA, AVION, PISICA, CAINE</strong>
             </li>
           </ul>
         </div>
       </div>
+      <div className="p-1">
+        <div className="lab-foldere-note p-1">
+          <div className="fs-4 mb-3 fw-bold text-center">
+            {`Pentru nota 8`.toUpperCase()}
+          </div>
+          <ul className="fs-5">
+            <li>
+              Deschide folderul <strong>LEGUME</strong> și creează următoarele{" "}
+              <strong>3</strong> foldere: <strong>CEAPA, ROSIE, CARTOFI</strong>
+            </li>
+            <li>
+              Navighează înapoi (apasă pe butonul{" "}
+              <i className="fa-solid fa-arrow-left"></i>) din colțul stânga sus
+            </li>
+            <li>
+              Deschide folderul <strong>FRUCTE</strong> și creează următoarele{" "}
+              <strong>3</strong> foldere: <strong>MERE, PERE, BANANE</strong>
+            </li>
+            <li>
+              {" "}
+              Navighează înapoi (apasă pe butonul{" "}
+              <i className="fa-solid fa-arrow-left"></i>) din colțul stânga sus
+            </li>
+            <li>
+              Copiază folderul <strong>PISICA</strong> in folderul{" "}
+              <strong>PERE</strong> (pentru a ajunge în folderul{" "}
+              <strong>PERE</strong> trebuie să navighezi înapoi{" "}
+              <i className="fa-solid fa-arrow-left"></i> în folderul{" "}
+              <strong>FRUCTE</strong> și apoi să intri în folderul{" "}
+              <strong>PERE</strong>)
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="p-1">
+        <div className="lab-foldere-note p-1">
+          <div className="fs-4 mb-3 fw-bold text-center">
+            {`Pentru nota 10`.toUpperCase()}
+          </div>
+          <ul className="fs-5">
+            <li>Navighează înapoi în folderul cu numele tău</li>
+            <li>
+              Mută folderul <strong>CAINE</strong> in folderul{" "}
+              <strong>ROSIE</strong>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="mt-2">
+        <ul className="fs-5 fw-semibold">
+          <li>După ce ai terminat cheamă-l pe domnul profesor</li>
+          <li>
+            Redenumește folderul <strong>AVION</strong> cu numele{" "}
+            <strong>LACTATE</strong>
+          </li>
+          <li>
+            Șterge folderul <strong>MASINA</strong>
+          </li>
+        </ul>
+      </div>
+
       {/* AJUTOR */}
       <div className="lab-foldere-ajutor">
         <div className="lab-foldere-ajutor-titlu text-center fw-bold fs-3 p-2 mb-3">
@@ -136,7 +132,7 @@ const LaboratorFoldere = () => {
             <img
               src={select_folder}
               alt="Select a folder video"
-              className="w-75"
+              className="w-100"
             />
           </div>
         </div>
@@ -155,7 +151,7 @@ const LaboratorFoldere = () => {
             <img
               src={create_folder}
               alt="Create a folder video"
-              className="w-75"
+              className="w-100"
             />
           </div>
         </div>
@@ -179,7 +175,7 @@ const LaboratorFoldere = () => {
             <img
               src={rename_folder}
               alt="Rename a folder video"
-              className="w-75"
+              className="w-100"
             />
           </div>
         </div>
@@ -199,7 +195,7 @@ const LaboratorFoldere = () => {
             <img
               src={delete_folder}
               alt="Delete a folder video"
-              className="w-75"
+              className="w-100"
             />
           </div>
         </div>
@@ -220,7 +216,11 @@ const LaboratorFoldere = () => {
             <strong>DESTINAȚIE</strong>
           </div>
           <div className="pos-c">
-            <img src={copy_folder} alt="Copy a folder video" className="w-75" />
+            <img
+              src={copy_folder}
+              alt="Copy a folder video"
+              className="w-100"
+            />
           </div>
         </div>
         {/* PASTE */}
@@ -239,7 +239,11 @@ const LaboratorFoldere = () => {
             află doar în folderul <strong>DESTINAȚIE</strong>.
           </div>
           <div className="pos-c">
-            <img src={move_folder} alt="Move a folder video" className="w-75" />
+            <img
+              src={move_folder}
+              alt="Move a folder video"
+              className="w-100"
+            />
           </div>
         </div>
       </div>
--- src/components/laborator/laboratorUkraine.jsx
+++ src/components/laborator/laboratorUkraine.jsx
@@ -6,94 +6,92 @@ const LaboratorUkraine = () => {
       <div className="lab-foldere-ajutor-titlu text-center fw-bold fs-3 p-2 mb-3">
         {`УКРАЇНСЬКА`.toUpperCase()}
       </div>
-      <div className="row m-0">
-        <div className="fs-4 fw-semibold">
-          Уважно прочитайте інструкцію (Ви можете скористатися Інтернетом, якщо
-          ви забули, як: вибрати, створити, перейменувати, видалити, копіювати,
-          перемістити папку.):
+      <div className="fs-4 fw-semibold">
+        Уважно прочитайте інструкцію (Ви можете скористатися Інтернетом, якщо ви
+        забули, як: вибрати, створити, перейменувати, видалити, копіювати,
+        перемістити папку.):
+        <ul className="fs-5">
+          <li>
+            Натисніть кнопку СТАРТ(
+            <i className="fa-brands fa-microsoft fs-4 mx-2"></i>{" "}
+            <strong>або</strong>{" "}
+            <i className="fa-brands fa-windows fs-4 mx-2"></i> ) у лівому куті
+            екрана.
+          </li>
+          <li>
+            Напишіть / знайдіть FILE EXPLORER і відкрийте його (клацніть на
+            ньому лівою кнопкою миші)
+          </li>
+          <li>
+            Ліворуч, у вузькій смузі, є список з кількох ярликів. Відкрийте
+            робочий стіл (клацніть на ньому лівою кнопкою миші).
+          </li>
+          <li>
+            Після відкриття <strong>DESKTOP</strong> створіть папку з такою
+            назвою: NAME FIRST NAME CLASS. (приклад: МАКСИМ РАДУ 8А)
+          </li>
+        </ul>
+      </div>
+      <div className="p-1">
+        <div className="lab-foldere-note p-1">
+          <div className="fs-4 mb-3 fw-bold text-center">
+            {`Для 6 класу`.toUpperCase()}
+          </div>
+          <ul className="fs-5">
+            <li>Відкрийте папку, створену під вашим іменем</li>
+            <li>
+              У папці зі своїм іменем створіть 6 папок з такими назвами: Nr 1,
+              Nr 2, Nr 3, Nr 4, N 5, Nr 6
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="p-1">
+        <div className="lab-foldere-note p-1">
+          <div className="fs-4 mb-3 fw-bold text-center">
+            {`Для 8 класу`.toUpperCase()}
+          </div>
           <ul className="fs-5">
             <li>
-              Натисніть кнопку СТАРТ(
-              <i className="fa-brands fa-microsoft fs-4 mx-2"></i>{" "}
-              <strong>або</strong>{" "}
-              <i className="fa-brands fa-windows fs-4 mx-2"></i> ) у лівому куті
-              екрана.
+              Відкрийте папку №1 і створіть наступні 3 папки: AAA, BBB, CCC
             </li>
             <li>
-              Напишіть / знайдіть FILE EXPLORER і відкрийте його (клацніть на
-              ньому лівою кнопкою миші)
+              Перейти назад (натисніть кнопку у верхньому лівому куті{" "}
+              <i className="fa-solid fa-arrow-left"></i>)
             </li>
             <li>
-              Ліворуч, у вузькій смузі, є список з кількох ярликів. Відкрийте
-              робочий стіл (клацніть на ньому лівою кнопкою миші).
+              Відкрийте папку № 2 і створіть наступні 3 папки: FFF, GGG, HHH
             </li>
             <li>
-              Після відкриття <strong>DESKTOP</strong> створіть папку з такою
-              назвою: NAME FIRST NAME CLASS. (приклад: МАКСИМ РАДУ 8А)
+              {" "}
+              Перейти назад (натисніть кнопку у верхньому лівому куті{" "}
+              <i className="fa-solid fa-arrow-left"></i>)
+            </li>
+            <li>
+              Скопіюйте папку № 6 до папки BBB (щоб отримати доступ до папки
+              BBB, потрібно повернутися до папки № 1, а потім увійти в папку
+              BBB)
             </li>
           </ul>
         </div>
-        <div className="col-lg-4 p-1">
-          <div className="lab-foldere-note p-1">
-            <div className="fs-4 mb-3 fw-bold text-center">
-              {`Для 6 класу`.toUpperCase()}
-            </div>
-            <ul className="fs-5">
-              <li>Відкрийте папку, створену під вашим іменем</li>
-              <li>
-                У папці зі своїм іменем створіть 6 папок з такими назвами: Nr 1,
-                Nr 2, Nr 3, Nr 4, N 5, Nr 6
-              </li>
-            </ul>
+      </div>
+      <div className="p-1">
+        <div className="lab-foldere-note p-1">
+          <div className="fs-4 mb-3 fw-bold text-center">
+            {`Для 10 класу`.toUpperCase()}
           </div>
-        </div>
-        <div className="col-lg-4 p-1">
-          <div className="lab-foldere-note p-1">
-            <div className="fs-4 mb-3 fw-bold text-center">
-              {`Для 8 класу`.toUpperCase()}
-            </div>
-            <ul className="fs-5">
-              <li>
-                Відкрийте папку №1 і створіть наступні 3 папки: AAA, BBB, CCC
-              </li>
-              <li>
-                Перейти назад (натисніть кнопку у верхньому лівому куті{" "}
-                <i className="fa-solid fa-arrow-left"></i>)
-              </li>
-              <li>
-                Відкрийте папку № 2 і створіть наступні 3 папки: FFF, GGG, HHH
-              </li>
-              <li>
-                {" "}
-                Перейти назад (натисніть кнопку у верхньому лівому куті{" "}
-                <i className="fa-solid fa-arrow-left"></i>)
-              </li>
-              <li>
-                Скопіюйте папку № 6 до папки BBB (щоб отримати доступ до папки
-                BBB, потрібно повернутися до папки № 1, а потім увійти в папку
-                BBB)
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="col-lg-4 p-1">
-          <div className="lab-foldere-note p-1">
-            <div className="fs-4 mb-3 fw-bold text-center">
-              {`Для 10 класу`.toUpperCase()}
-            </div>
-            <ul className="fs-5">
-              <li>Поверніться до папки з вашим іменем</li>
-              <li>Папку No 5 перемістити в папку HHH</li>
-            </ul>
-          </div>
-        </div>
-        <div className="mt-2">
-          <ul className="fs-5 fw-semibold">
-            <li>Коли ви закінчите, покличте вчителя</li>
-            <li>Перейменуйте папку № 3 на будь-яку назву.</li>
-            <li>Видалити папку №4</li>
+          <ul className="fs-5">
+            <li>Поверніться до папки з вашим іменем</li>
+            <li>Папку No 5 перемістити в папку HHH</li>
           </ul>
         </div>
+      </div>
+      <div className="mt-2">
+        <ul className="fs-5 fw-semibold">
+          <li>Коли ви закінчите, покличте вчителя</li>
+          <li>Перейменуйте папку № 3 на будь-яку назву.</li>
+          <li>Видалити папку №4</li>
+        </ul>
       </div>
     </>
   );
